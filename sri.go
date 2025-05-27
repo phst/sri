f27dbe2e358857637d60cbaea56f2060f68e01ac
Philipp Stephani <p.stephani2@gmail.com>
2025-05-27 09:19:54 +0200

    Skip printing on failure

--- sri.go
+++ sri.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Philipp Stephani
+// Copyright 2023, 2025 Philipp Stephani
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -68,6 +68,7 @@ func main() {
 		if r.err != nil {
 			fmt.Fprintf(os.Stderr, "sri: %s: %s\n", r.file, r.err)
 			ok = false
+			continue
 		}
 		var s strings.Builder
 		s.WriteString(hashName)
